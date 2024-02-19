e6531b208da1b38c9965879cf2a0714c15a3b935
Leonardo Oliveira <leooliveira1025@gmail.com>
2024-02-19 08:53:53 +0000

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
       <div className="Footer fixed bottom-0 left-0 bg-gradient-to-r from-blue-300 to-light-blue-400 w-screen h-12">
         <p className="text-gray-800 font-bold sticky top-[100vh] text-center mb-3">
-          <a href="https://github.com/lmcoliveira99/To-Do-React-App">
+          <a href="https://github.com/lmcoliveira99/Todo-App">
             You can find the source code here
           </a>
         </p>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,23 +4,22 @@ function About() {
         <h1 className="uppercase text-2xl font-bold mb-8"> About Us</h1>
   
         <div className="flex justify-center items-center flex-wrap">
-          {/* Card for Leonardo Oliveira */}
+          
           <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-gradient-to-b from-blue-300 to-light-blue-400 p-8 flex flex-col items-center">
             <img
-              src="https://placekitten.com/300/200"
+              src="https://avatars.githubusercontent.com/u/116581678?v=4"
               alt="Leonardo Oliveira"
               className="w-48 h-48 object-cover rounded-full mb-4"
             />
             <div className="px-6 py-4 text-center">
               <div className="font-bold text-xl mb-2 text-grey-800">Leonardo Oliveira</div>
               <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula aliquet
-                elit, in ultrices est fermentum eu.
+                Full-Stack Web Developer
               </p>
             </div>
             <div className="px-6 py-4">
               <a
-                href="https://github.com/leonardo"
+                href="https://github.com/lmcoliveira99"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-500 font-bold"
@@ -30,23 +29,22 @@ function About() {
             </div>
           </div>
   
-          {/* Card for André Rodrigues */}
+          
           <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-gradient-to-b from-blue-300 to-light-blue-400 p-8 flex flex-col items-center">
             <img
-              src="https://placekitten.com/300/200"
+              src="https://avatars.githubusercontent.com/u/155987682?v=4"
               alt="André Rodrigues"
               className="w-48 h-48 object-cover rounded-full mb-4"
             />
             <div className="px-6 py-4 text-center">
               <div className="font-bold text-xl mb-2 text-grey-800">André Rodrigues</div>
               <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula aliquet
-                elit, in ultrices est fermentum eu.
+              Full-Stack Web Developer
               </p>
             </div>
             <div className="px-6 py-4">
               <a
-                href="https://github.com/andre"
+                href="https://github.com/Andr3L4"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-500 font-bold"
